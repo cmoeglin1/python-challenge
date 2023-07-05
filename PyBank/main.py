c652414878b55b177d48c8e84850ba632e8034f0
cmoeglin1 <cmoeglin1@gmail.com>
2023-07-05 17:40:55 -0400

    first commit pypoll

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,19 +12,21 @@ data_df = ps.read_csv(csvpath)
 total_months = data_df["Date"].count()
 net_total = data_df["Profit/Losses"].sum()
 
-
+# calculate change in profit for each month and find average
 data_df["Profit/Losses Change"] = data_df["Profit/Losses"].diff()
-
 average_change = data_df["Profit/Losses Change"].mean()
 
+# find row with max change
 maxrow = data_df["Profit/Losses Change"].idxmax()
 max_date = data_df.loc[maxrow,"Date"]
 max_value = data_df.loc[maxrow,"Profit/Losses Change"]
 
+# find row with biggest loss
 minrow = data_df["Profit/Losses Change"].idxmin()
 min_date = data_df.loc[minrow,"Date"]
 min_value = data_df.loc[minrow,"Profit/Losses Change"]
 
+# print out results to terminal
 print("Financial Analysis")
 print("-------------------")
 print("Total Months: "+str(total_months))
@@ -32,3 +34,4 @@ print("Total: $"+str(net_total))
 print("Average Change: $"+str(average_change))
 print("Greatest Increase in Profits: "+max_date+" ($"+str(max_value)+")")
 print("Greatest Decrease in Profits: "+min_date+" ($"+str(min_value)+")")
+
---
+++ PyPoll/main.py
@@ -0,0 +1,34 @@
+import os
+import csv
+import pandas as ps
+
+# define path to csv file
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+# read csv, store as dataframe
+data_df = ps.read_csv(csvpath)
+
+# count total number of votes
+total_votes = data_df["Ballot ID"].count()
+
+# count votes for each candidate and store as dataframe
+counts_df = data_df["Candidate"].value_counts().to_frame()
+
+# fix column names 
+counts_df = counts_df.reset_index()
+counts_df.columns = ["Name", "Votes"]
+
+# find percentage each candidate received and store as column in dataframe
+percent = round((counts_df["Votes"] / total_votes) * 100, 2)
+counts_df["Percentage"] = percent
+
+# print the analysis to console
+print("Election Results")
+print("-----------------")
+print("Total Votes: "+str(total_votes))
+print("-----------------")
+for x in range(counts_df["Name"].count()):
+    print(counts_df["Name"].iloc[x] + ": "+ str(counts_df["Percentage"].iloc[x]) + "% ("+ str(counts_df["Votes"].iloc[x]) + ")")
+print("-----------------")
+print("Winner: "+counts_df["Name"].iloc[0])
+print("-----------------")
